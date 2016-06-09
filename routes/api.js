6757da532348230ef7d74b5af86c106d38587455
alejandrorc93 <root@debian>
2016-06-09 09:34:09 -0400

    First api commit

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,136 @@
 
 'use strict';
 
-const express    = require('express');
-const router     = express.Router();
+const express = require('express');
+const router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
+var Client  = require('../models/client');
+var Admin = require('../models/admin');
+var Product = require('../models/product');
 
 module.exports = router;
 
+//register
+function sendVerification(){
+	console.log("Sending verification...");
+}
+
+//send id writtem by the user in the body
+router.put('/verify',(req, res) => {
+	Client.findOne({_id: new ObjectId(req.body._id)},
+	(err, client) => {
+		if(!err) console.log(client + ' found.');
+		client.verified = true;		
+		client.save((err, obj) => {
+			if (!err) {
+				console.log(obj.name + ' saved.');  		
+				res.json(client);
+			}  	
+  	});
+	});
+});
+
+//send clients data in the body
+router.post('/register', (req, res) => {
+	var address = {
+  	street: req.body.street,
+  	postalCode: req.body.postalCode,
+  	number: req.body.number,
+  	state: req.body.state,
+  	city: req.body.city
+	}	
+	
+	var client = new Client();
+  client.name = req.body.name;
+  client.lastName = req.body.lastName;
+  client.email = req.body.email;
+  client.passwordDigest = req.body.password;
+  client.verified = false;
+  client.address = address;
+  client.cards = [];
+  client.cart = {
+  	orders: [],
+  	discount: 0
+  }
+  
+  client.save((err, obj) => {
+  	if (!err) {
+  		console.log(obj.name + 'saved.');  		
+  		sendVerification();
+  		res.json(client);
+  	}  	
+  }); 
+});
+
+//client login send emal and password in the body
+router.post('/clientLogin', (req, res) => {
+	Client.findOne(
+		{email: req.body.email, 
+		passwordDigest: req.body.password}, 
+	(err, client) => {
+		if(!err)console.log(client + 'found.');
+		res.json(client);		
+	});	
+});
+
+//admin login send email and password in the body
+router.post('/adminLogin', (req, res) => {
+	Admin.findOne(
+		{email: req.body.email, 
+		password: req.body.password}, 
+	(err, admin) => {
+		if(!err)console.log(admin + 'found.');
+		res.json(admin);		
+	});	
+});
+
+//get product information send product _id as a parameter
+router.get('/productInfo', (req, res) => {
+	Product.findOne({_id: new ObjectId(req.param('id'))},
+	(err, product) => {
+		if(!err) console.log(product + ' found.');
+		res.json(product);
+	});
+});
+
+//get client information send client _id as a parameter
+router.get('/clientInfo', (req, res) => {
+	Client.findOne({_id: new ObjectId(req.param('_id'))},
+	(err, client) => {
+		if(!err) console.log(client + ' found.');
+		res.json(client);
+	});
+});
+
+//modifiy client information send client updated information in the body
+//does not change email and password
+router.put('/modifyClient', (req, res) => {
+	Client.findOne({_id: new ObjectId(req.body._id)},
+	(err, client) => {
+		if(!err) console.log(client + ' found.');
+		client.name = req.body.name;
+		client.lastName = req.body.lastName;
+		client.address.street = req.body.street,
+  	client.address.postalCode = req.body.postalCode,
+  	client.address.number = req.body.number,
+  	client.address.state = req.body.state,
+  	client.address.city = req.body.city
+		client.cards = req.body.cards;
+		
+		client.save((err, obj) => {
+			if (!err) {
+				console.log(obj.name + ' saved.');  		
+				sendVerification();
+				res.json(client);
+			}  	
+  	});
+	});
+});
+
 //Test
-router.get('/', (req, res) => {	
+router.get('/test', (req, res) => {	
+  var n = req.params('name');
   res.json({
-    name: 'Bob'
+    name: n
   });
 });
