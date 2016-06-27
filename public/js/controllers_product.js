ef48a5f2c18b3b25c57645b8edd3e5af3383e387
Diego Monroy <diego.monroy.f@gmail.com>
2016-06-26 21:59:16 -0500

    New main menu which lists available products. Clients can now be deleted also by a SU. Salesman admin implemented.

--- public/js/controllers_product.js
+++ public/js/controllers_product.js
@@ -7,7 +7,32 @@ var route = "http://localhost:3000/api";
 angular.module('myApp.controllers_product', []).
   controller('Controller_MainMenu', function ($scope, $http, $state, $stateParams) {
   	// Controller that is used for displaying the available items when on the main page.
-  	$scope.items = ['banana', 'platano', 'manzana', 'naranja', 'pera', 'mandarina'];
+
+    // Method to get all available products.
+    $scope.getProducts = function() {
+      $http.get(route + '/products')
+      .then(function succeedCallbackFunction(response) {
+        $scope.rows = response.data;
+        // To order by groups of 3.
+        var arrays = [], size = 3;
+        while ($scope.rows.length > 0) {
+          arrays.push($scope.rows.splice(0, size));
+        }
+        console.log(arrays[0][0].image);
+        $scope.rows = arrays;
+      }, function errorCallbackFunction(response) {
+        console.log('Couldn\'t make products petition.');
+      });
+    };
+
+    // Method to view one specific product's details.
+    $scope.loadProductDetails = function() {
+      $state.go('product_details', {
+        product_id: $scope.product_id
+      });
+    };
+
+    $scope .getProducts();
 
   }).
   controller('Controller_ProductDetail', function ($scope) {
--- public/js/directives.js
+++ public/js/directives.js
@@ -8,3 +8,28 @@ angular.module('myApp.directives', []).
       elm.text(version);
     };
   });
+
+angular.module('myApp').directive('gridDirective', function() {
+	return {
+		restrict: 'AEC',
+		replace: true,
+		templateUrl: 'partials/main_menu_grid.html',
+		link: function(scope, elem, attrs) {
+			console.log('Directive called.');
+		}
+	}
+});
+
+angular.module('myApp.directives', []).
+	directive('ngIf', function() {
+    return {
+      link: function(scope, element, attrs) {
+        if(scope.$eval(attrs.ngIf)) {
+            // remove '<div ng-if...></div>'
+            element.replaceWith(element.children())
+        } else {
+            element.replaceWith(' ')
+        }
+      }
+    }
+});
